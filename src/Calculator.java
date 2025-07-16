74d3123ed41809228bdd241ce1f892d3ebaf6505
suspendedbelief1iii <jayglee11@gmail.com>
2025-07-15 22:16:44 -0700

    add: JOptionPane implementation

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Calculator {
 
     /*
      * This class represents the GUI for the calculator.
+     * It uses JOptionPane to interact with the user.
      * It takes user input for two numbers and an operation,
      * performs the operation, and outputs the result.
      * Does not support decimal exponents like roots, unfortunately.
@@ -11,41 +12,25 @@ public class Calculator {
 
     public static void main(String[] args) throws Exception {
 
-        // Create input scanner object.
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello, welcome to Calculator!");
+        // Welcome user to the calculator.
+        JOptionPane.showMessageDialog(null, "Welcome to Calculator!");
 
         // Take user's first number input.
-        System.out.println("Please input your first number.");
-        double a = scanner.nextDouble();  // Scans line for the first value.
-        if (a % 1.0 == 0) {
-            System.out.println("You entered " + ((int)a) + ".");
-        } else {
-            System.out.println("You entered " + a + ".");
-        }
-        scanner.nextLine();  // Clear input line.
+        double a = Double.parseDouble(JOptionPane.showInputDialog(null, "<html><code></code><br>Enter first number:</html>"));
 
         // Take user's operation input.
-        String operation = "";
+        String operation;
         do {
-            System.out.println("Please input your operation (+, -, *, /, ^). ");
-            operation = scanner.nextLine();  // Scans line for the operation.
+            operation = JOptionPane.showInputDialog(null, "<html><code>" + a + "</code><br>Enter operation (+, -, *, /, ^):</html>");
 
             // If input is not a valid operation, tell user and retry.
             if (!operation.equals("+") && !operation.equals("-") && !operation.equals("*") && !operation.equals("/") && !operation.equals("^")) {
-                System.out.println("Invalid operation. Please pick out of the following: +, -, *, /, ^. ");
+                JOptionPane.showMessageDialog(null, "Invalid operation. Please pick out of the following: +, -, *, /, ^. ");
             }
         } while (!operation.equals("+") && !operation.equals("-") && !operation.equals("*") && !operation.equals("/") && !operation.equals("^"));
-        System.out.println("You entered operation " + operation + ".");
 
         // Take user's second number input.
-        System.out.println("Please input your second number.");
-        double b = scanner.nextDouble();  // Scans line for the second value.
-        if (b % 1.0 == 0) {
-            System.out.println("You entered " + ((int)b) + ".");
-        } else {
-            System.out.println("You entered " + b + ".");
-        }
+        double b = Double.parseDouble(JOptionPane.showInputDialog(null, "<html><code>" + a +  " " + operation + "</code><br>Enter second number:</html>"));
 
         // Based on input operation, perform calculation.
         double answer = 0;
@@ -68,13 +53,11 @@ public class Calculator {
         }
         
         // Is answer is floorable without losing significant digits, floor. Then print answer.
+        String answerMsg = "<html><code>" + a +  " " + operation + " " + b + " = " + answer + "</code><br>The answer is: </html>";
         if (answer % 1.0 == 0) {
-            System.out.println("The answer is " + ((int)answer) + ".");
+            JOptionPane.showMessageDialog(null, answerMsg + ((int)answer));
         } else {
-            System.out.println("The answer is " + answer + ".");
+            JOptionPane.showMessageDialog(null, answerMsg + answer);
         }
-
-        // Close input scanner.
-        scanner.close();
     }
 }
